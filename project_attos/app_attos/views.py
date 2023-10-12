50def1e334b65ddeaac1e634069ad49ca5af51fe
Talita Fraga <talitafraga@MacBook-Air-de-Talita.local>
2023-10-11 22:47:20 -0300

    inicializando projeto

---
+++ project_attos/app_attos/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.shortcuts import get_object_or_404, render
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "app_attos/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "app_attos/detail.html", {"question": question})
+
+
+
+
