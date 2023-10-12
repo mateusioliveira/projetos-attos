70550f6e073b0727586cbb90a545334feba15be9
Talita Fraga <talitafraga@MacBook-Air-de-Talita.local>
2023-10-12 20:23:02 -0300

    merge

--- project_attos/attos/urls.py
+++ project_attos/attos/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("app_attos/", include("app_attos.urls")),
+    path("", include("app_attos.urls")),
     path('admin/', admin.site.urls),
 ]
