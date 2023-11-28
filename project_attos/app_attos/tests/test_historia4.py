1ca6ac3779e17b3d5f37280396000f6cc6ea52e6
victor guilherme <guilherme@pop-os.localdomain>
2023-11-28 18:12:32 -0300

    add test04

---
+++ project_attos/app_attos/tests/test_historia4.py
@@ -0,0 +1,43 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=options)
+
+
+class Historia4(LiveServerTestCase):
+    def test_01(self):
+        driver.get("http://127.0.0.1:8000")
+        try:
+            email = driver.find_element(By.CSS_SELECTOR, "input[name='email']")
+            senha = driver.find_element(By.CSS_SELECTOR, "input[name='senha']")
+        except NoSuchElementException:
+            print("Email or senha element not found.")
+            return
+
+        email.send_keys("teste25@teste.com")
+        senha.send_keys("123")
+        entrar = driver.find_element(By.XPATH, "//button[@value='Entrar']")
+        time.sleep(2)
+        entrar.click()
+        time.sleep(2)
+        adicionar= driver.find_element(By.XPATH, "//button[@class='adicionar']")
+        adicionar.click()
+        foto= driver.find_element(By.XPATH, "//input[@name='foto']")
+        time.sleep(2)
+        foto.send_keys("/home/guilherme/Pictures/teste.png")
+        time.sleep(2)
+        botao_enviar = driver.find_element(By.XPATH, "//button[@name='enviar']")
+        botao_enviar.click()
+        confirmar_enviar= driver.find_element(By.XPATH, "//button[@name='enviar']")
+        self.assertTrue(confirmar_enviar.is_enabled())     
