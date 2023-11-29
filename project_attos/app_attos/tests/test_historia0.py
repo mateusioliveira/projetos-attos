45074a6d7fedaf55fc8a1296fc54b5f9f80ce610
João Silva <vitoriasilva8316@gmail.com>
2023-11-29 10:30:28 -0300

    teste cadastro

--- project_attos/app_attos/tests/test_historia0.py
+++ project_attos/app_attos/tests/test_historia0.py
@@ -9,9 +9,9 @@ import time
 
 
 options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
 
 class Historia4(LiveServerTestCase):
