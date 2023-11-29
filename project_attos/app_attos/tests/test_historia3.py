faee2cd64cda938be143077d0a7150447df6bec6
victor guilherme <guilherme@pop-os.localdomain>
2023-11-29 16:38:55 -0300

    corrigindo test3

--- project_attos/app_attos/tests/test_historia3.py
+++ project_attos/app_attos/tests/test_historia3.py
@@ -6,12 +6,7 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(options=options)
+driver=webdriver.Firefox()
 class Historia3(LiveServerTestCase):
     def test_01(self):
         driver.get("http://127.0.0.1:8000")
@@ -24,9 +19,11 @@ class Historia3(LiveServerTestCase):
 
         email.send_keys("teste25@teste.com")
         senha.send_keys("123")
-        entrar = driver.find_element(By.XPATH, "//button[@value='Entrar']")
-        time.sleep(2)
+        entrar = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".botton-entrar button"))
+        )
         entrar.click()
+
         time.sleep(2)
         adicionar= driver.find_element(By.XPATH, "//button[@class='adicionar']")
         adicionar.click()
