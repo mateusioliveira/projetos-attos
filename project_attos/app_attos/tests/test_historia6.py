4528224e6e7d82fd0e5d8e461b4cb4003f890b0b
juliafelixcor <98843736+juliafelixcor@users.noreply.github.com>
2023-11-29 23:26:15 -0300

    teste hisitoria 6 ajuste

--- project_attos/app_attos/tests/test_historia6.py
+++ project_attos/app_attos/tests/test_historia6.py
@@ -8,7 +8,7 @@ import time
 
 
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
